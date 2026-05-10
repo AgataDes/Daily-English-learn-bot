c78cf1296b592e47358c93286bfb61f6702b03e2
AgataDes <agatadesecka@wp.pl>
2026-05-10 20:44:39 +0200

    Add links

--- script_ang.py
+++ script_ang.py
@@ -4,6 +4,12 @@ import random
 from deep_translator import GoogleTranslator
 
 
+deepl_link = f"https://www.deepl.com/write"
+reverso_link = f"https://context.reverso.net/translation/english-polish/{word}"
+
+
+
+
 DISCORD_WEBHOOK_URL = os.getenv('DISCORD_WEBHOOK')
 
 def fetch_data():
@@ -15,7 +21,14 @@ def fetch_data():
     chosen_line = random.choice(lines)
     word = chosen_line.split()[0]
     translation = GoogleTranslator(source='en', target='pl').translate(word)
-    message = f"Oto Twoje słowo na dziś: **{word}**\nPo polsku: **{translation}**"
+    message = (
+        f"### 🇬🇧 Word of the Day: **{word.upper()}**\n"
+        f"🇵🇱 **Tłumaczenie:** {translation}\n"
+        f"--- \n"
+        f"💪 **Twoje zadanie:** Ułóż zdanie z tym słowem i sprawdź je tutaj:\n"
+        f"👉 [DeepL Write - Popraw błędy]({deepl_link})\n"
+        f"👉 [Reverso Context - Przykłady użycia]({reverso_link})"
+    )
     return message
 
 
